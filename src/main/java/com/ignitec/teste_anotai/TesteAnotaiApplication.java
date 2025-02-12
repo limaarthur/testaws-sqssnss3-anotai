07074be6841cd89e67ec5552a193e2e8d0a7b56a
limaarthur <artglima.dev@gmail.com>
2025-02-12 13:28:56 -0300

    first commit

---
+++ src/main/java/com/ignitec/teste_anotai/TesteAnotaiApplication.java
@@ -0,0 +1,13 @@
+package com.ignitec.teste_anotai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAnotaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAnotaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignitec/teste_anotai/TesteAnotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignitec.teste_anotai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAnotaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
